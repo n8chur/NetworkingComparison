ababef7d270c666257ff2d6156632ebe41c98989
Ian Luo <lokae0@gmail.com>
2019-10-25 14:07:58 -0700

    Initial Commit

---
+++ NetworkingComparison/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NetworkingComparison
+//
+//  Created by Ian Luo on 10/25/19.
+//  Copyright © 2019 Lokae. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
