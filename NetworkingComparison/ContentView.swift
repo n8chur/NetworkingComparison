88763d11d232645058f3914fb96b54e134c4b09b
Ian Luo <lokae0@gmail.com>
2019-10-28 23:40:35 -0700

    Adjust SwiftUI navigation & UI

--- NetworkingComparison/ContentView.swift
+++ NetworkingComparison/ContentView.swift
@@ -9,23 +9,24 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var isUrlSessionModalVisible = false
+    @State private var showUrlSessionView: Int? = 0
 
     var body: some View {
         NavigationView {
-            VStack(spacing: 40.0) {
+            VStack(alignment: .leading, spacing: 40.0) {
+                NavigationLink(destination: URLSessionView(), tag: 1, selection: $showUrlSessionView) {
+                    EmptyView().frame(width: 0, height: 0, alignment: .center)
+                }
+
                 HStack(spacing: 40.0) {
                     Button(action: {
-                        self.isUrlSessionModalVisible = true
+                        self.showUrlSessionView = 1
                     }) {
                         Text("URLSession")
                     }
                     .padding()
                     .background(Color.blue)
                     .cornerRadius(20.0)
-                    .sheet(isPresented: $isUrlSessionModalVisible) {
-                        URLSessionView()
-                    }
 
                     Button(action: {
                         print("Hello")
@@ -56,6 +57,8 @@ struct ContentView: View {
                     .background(Color.blue)
                     .cornerRadius(20.0)
                 }
+
+                Spacer()
             }
             .foregroundColor(Color.white)
             .font(.title)
